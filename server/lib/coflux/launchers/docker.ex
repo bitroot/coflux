f9af760e78a28237b5c322d24279feed893e2d3b
Joe Freeman <joe@joef.uk>
2025-04-27 17:44:48 +0100

    Update docs

--- server/lib/coflux/launchers/docker.ex
+++ server/lib/coflux/launchers/docker.ex
@@ -1,5 +1,5 @@
 defmodule Coflux.DockerLauncher do
-  def launch(project_id, environment_name, agent_id, repositories, provides, config \\ %{}) do
+  def launch(project_id, workspace_name, agent_id, repositories, provides, config \\ %{}) do
     # TODO: option to configure docker host/socket?
     # TODO: option to configure coflux host?
     with {:ok, %{"Id" => container_id}} <-
@@ -9,7 +9,7 @@ defmodule Coflux.DockerLauncher do
              "Cmd" => repositories,
              "Env" => [
                "COFLUX_HOST=localhost:7777",
-               "COFLUX_ENVIRONMENT=#{environment_name}",
+               "COFLUX_WORKSPACE=#{workspace_name}",
                "COFLUX_PROJECT=#{project_id}",
                "COFLUX_LAUNCH=#{agent_id}",
                "COFLUX_PROVIDES=#{encode_provides(provides)}"
