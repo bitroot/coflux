e06562cd628650a9b2b6efd713a85c7f0e30f276
Joe Freeman <joe@joef.uk>
2025-05-11 14:53:42 +0100

    Remove commented line

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,8 +2,6 @@ import { createElement } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
-// import "./style.css";
-
 const container = document.getElementById("root");
 const root = createRoot(container!);
 root.render(createElement(App));
