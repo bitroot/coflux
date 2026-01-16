25b9fa32e3b0a1b67740196a5a86d17f1772bb8c
Joe Freeman <joe@joef.uk>
2026-01-16 21:10:05 +0000

    Return separate API version

--- server/lib/coflux/handlers/well_known.ex
+++ server/lib/coflux/handlers/well_known.ex
@@ -1,14 +1,20 @@
 defmodule Coflux.Handlers.WellKnown do
   import Coflux.Handlers.Utils
 
+  @version Mix.Project.config()[:version]
+  @api_version (case Version.parse(@version) do
+                  {:ok, %Version{major: 0, minor: minor}} -> "0.#{minor}"
+                  {:ok, %Version{major: major}} -> "#{major}"
+                  :error -> @version
+                end)
+
   def init(req, opts) do
     req = handle(req, :cowboy_req.method(req))
     {:ok, req, opts}
   end
 
   defp handle(req, "GET") do
-    version = Application.spec(:coflux, :vsn) |> to_string()
-    json_response(req, %{"version" => version})
+    json_response(req, %{"version" => @version, "apiVersion" => @api_version})
   end
 
   defp handle(req, _method) do
